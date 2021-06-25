c5d5463770c44db46c76dd73a3c48ead8524666a
shervin zadsoroor <shervinzadsoroor@gmail.com>
2021-06-25 22:00:28 +0430

    implemented editable table data

--- src/main/java/com/logicbig/example/service/PersonService.java
+++ src/main/java/com/logicbig/example/service/PersonService.java
@@ -2,8 +2,12 @@ package com.logicbig.example.service;
 
 import com.logicbig.example.model.Person;
 import com.logicbig.example.repository.PersonDao;
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
 import org.springframework.stereotype.Service;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import java.util.List;
 
 @Service
@@ -24,5 +28,25 @@ public class PersonService {
                 person.setIsNationalCodeValid(false);
         });
         return people;
+    }
+
+    public void onRowEdit(RowEditEvent<Person> event) {
+        FacesMessage msg = new FacesMessage("person Edited", String.valueOf(event.getObject().getFirstName()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowCancel(RowEditEvent<Person> event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject().getFirstName()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+
+        if (newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
     }
 }
